026cc1f9b97dd698cb72085f7cd2378f415748da
AllanG <allang4779@gmail.com>
2022-06-22 15:25:33 +0300

    Update HomePage.java
    Fixed cors errors

--- src/main/java/com/group4/qrcodepayment/controller/HomePage.java
+++ src/main/java/com/group4/qrcodepayment/controller/HomePage.java
@@ -4,6 +4,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 @CrossOrigin(origins = "*")
