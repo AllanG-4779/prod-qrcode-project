269cadec52eb8f5b8f717c58f68c4b02239115dc
AllanG-4779 <allang4779@gmail.com>
2022-06-23 17:51:13 +0300

    changed the url constants

--- src/main/java/com/group4/qrcodepayment/util/URLs.java
+++ src/main/java/com/group4/qrcodepayment/util/URLs.java
@@ -3,5 +3,10 @@ package com.group4.qrcodepayment.util;
 public class URLs {
     public static final String REGISTER = "/auth/register";
     public static final String LOGIN = "/auth/login";
-    public static final String HOME = "/insights";
+    public static final String HOME = "/insights/home";
+
+    public static final String ASSERT_REGISTRATION = "/auth/registration/verify";
+    public static final String SEND_OTP = "/auth/otp/send";
+    public static final String VALIDATE_OTP = "/auth/otp/validate";
+
 }
