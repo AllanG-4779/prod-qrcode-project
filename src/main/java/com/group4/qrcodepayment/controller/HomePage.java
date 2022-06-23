835e3e9576ca4d3344abf8b4dcbadc7bf18276ca
AllanG-4779 <allang4779@gmail.com>
2022-06-23 09:57:34 +0300

    removed the conflicting annotations

--- src/main/java/com/group4/qrcodepayment/controller/HomePage.java
+++ src/main/java/com/group4/qrcodepayment/controller/HomePage.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 @RequestMapping("/insights")
-@RestController
+
 
 @CrossOrigin(origins = "*")
 public class HomePage {
