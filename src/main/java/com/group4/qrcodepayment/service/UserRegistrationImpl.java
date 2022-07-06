5348efa5196b6043616a5aac8f752ee3a0584b17
allang <allang4779@gmail.com>
2022-07-06 14:22:43 +0300

    fixed the json error in heroku

--- src/main/java/com/group4/qrcodepayment/service/UserRegistrationImpl.java
+++ src/main/java/com/group4/qrcodepayment/service/UserRegistrationImpl.java
@@ -8,14 +8,10 @@ import com.group4.qrcodepayment.models.UserInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.boot.configurationprocessor.json.JSONObject;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.sql.SQLException;
 import java.util.Optional;
-
 import java.util.UUID;
 
 @Service
