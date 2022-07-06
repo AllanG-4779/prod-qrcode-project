40d952eec996300eb8157118188c0369b30da6a8
allang <allang4779@gmail.com>
2022-07-06 14:33:07 +0300

    changed the pom.xml to remove the json dependency

--- src/main/java/com/group4/qrcodepayment/controller/Auth.java
+++ src/main/java/com/group4/qrcodepayment/controller/Auth.java
@@ -16,7 +16,7 @@ import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.configurationprocessor.json.JSONException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -67,7 +67,8 @@ public class Auth {
 
             response = RegistrationDto.class
     )
-    public ResponseEntity<Object> register(@RequestBody @Valid RegistrationDto details ) throws PhoneOrEmailExistsException, RegistrationFailedException, JSONException {
+    public ResponseEntity<Object> register(@RequestBody @Valid RegistrationDto details )
+            throws PhoneOrEmailExistsException, RegistrationFailedException{
 
 //        check if user is available
        userRegistrationService.checkUserNameExists(details.getPhone());
