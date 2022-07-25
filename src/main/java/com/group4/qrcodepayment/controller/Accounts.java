a77d7ae31f1b5ef45355006d972fd59d5fcc1bac
allang <allang4779@gmail.com>
2022-07-25 17:39:18 +0300

    Resolved JSON exception

--- src/main/java/com/group4/qrcodepayment/controller/Accounts.java
+++ src/main/java/com/group4/qrcodepayment/controller/Accounts.java
@@ -1,45 +1,26 @@
 package com.group4.qrcodepayment.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
 import com.google.zxing.WriterException;
 import com.group4.qrcodepayment.Repositories.BankRepo;
 import com.group4.qrcodepayment.Repositories.UserRepoInt;
 import com.group4.qrcodepayment.dto.AccountLinkingDto;
-import com.group4.qrcodepayment.dto.BankDto;
 import com.group4.qrcodepayment.exception.resterrors.AccountLinkFailedException;
 import com.group4.qrcodepayment.exception.resterrors.AuthenticationNotFoundException;
 import com.group4.qrcodepayment.exception.resterrors.UnsupportedBankException;
-import com.group4.qrcodepayment.models.Bank;
-import com.group4.qrcodepayment.models.QPayAccount;
 import com.group4.qrcodepayment.models.UserInfo;
 import com.group4.qrcodepayment.service.AccountServiceImpl;
 import com.group4.qrcodepayment.service.BankServiceImpl;
 import com.group4.qrcodepayment.util.QRCodeGenerator;
 import lombok.AllArgsConstructor;
-import lombok.Data;
-import org.apache.http.util.ByteArrayBuffer;
 import org.jasypt.util.text.AES256TextEncryptor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
-import org.springframework.boot.configurationprocessor.json.JSONException;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.ByteArrayHttpMessageConverter;
-import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.file.attribute.UserPrincipalNotFoundException;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
 
 @RestController
 @AllArgsConstructor
@@ -65,7 +46,7 @@ public class Accounts {
 
 
     @GetMapping("/myqr-code")
-    public byte[] generateMyQr(@RequestParam int width, @RequestParam int  height) throws IOException, WriterException, AuthenticationNotFoundException, JSONException {
+    public byte[] generateMyQr(@RequestParam int width, @RequestParam int  height) throws IOException, WriterException, AuthenticationNotFoundException {
     Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
     if(!authentication.isAuthenticated() || authentication instanceof AnonymousAuthenticationToken){
         throw new AuthenticationNotFoundException("Seems you are not authenticated, please login ");
@@ -87,10 +68,6 @@ public class Accounts {
 
 
 
-    }
-    @Bean
-    public ByteArrayHttpMessageConverter httpMessageConverter(){
-        return new ByteArrayHttpMessageConverter();
     }
 
 
--- src/main/java/com/group4/qrcodepayment/models/Account.java
+++ src/main/java/com/group4/qrcodepayment/models/Account.java
@@ -6,7 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.io.Serializable;
+
 
 @Entity
 @Data
